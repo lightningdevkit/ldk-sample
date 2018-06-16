6d605a0afb1d71411d50a05f0f8e6b5011aa9249
Matt Corallo <git@bluematt.me>
2018-06-16 15:22:51 -0400

    Set up events handling infra and stdin reading infra

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate secp256k1;
 extern crate bitcoin;
 extern crate tokio;
 extern crate tokio_io;
+extern crate tokio_fs;
 extern crate tokio_codec;
 extern crate bytes;
 
@@ -16,6 +17,7 @@ use hyper::{Client,StatusCode};
 use futures::future;
 use futures::future::Future;
 use futures::{AsyncSink, Stream, Sink};
+use futures::sync::mpsc;
 
 use secp256k1::key::{PublicKey, SecretKey};
 use secp256k1::Secp256k1;
@@ -24,6 +26,7 @@ use rand::{thread_rng, Rng};
 
 use lightning::ln::{peer_handler, router, channelmanager, channelmonitor};
 use lightning::chain::chaininterface;
+use lightning::util::events::EventsProvider;
 
 use bitcoin::network::constants;
 use bitcoin::network::serialize::BitcoinHash;
@@ -37,8 +40,6 @@ use std::os::unix::io::{RawFd, AsRawFd};
 use std::hash::Hash;
 use std::time::{Instant, Duration};
 
-use tokio::io::AsyncRead;
-
 const FEE_PROPORTIONAL_MILLIONTHS: u32 = 10;
 const ANNOUNCE_CHANNELS: bool = false;
 
@@ -46,6 +47,29 @@ const ANNOUNCE_CHANNELS: bool = false;
 fn _check_usize_is_64() {
 	// We assume 64-bit usizes here. If your platform has 32-bit usizes, wtf are you doing?
 	unsafe { mem::transmute::<*const usize, [u8; 8]>(panic!()); }
+}
+
+fn hex_to_compressed_pubkey(hex: &str) -> Option<PublicKey> {
+	let mut b = 0;
+	let mut data = Vec::with_capacity(33);
+	for (idx, c) in hex.as_bytes().iter().enumerate() {
+		if idx >= 33*2 { break; }
+		b <<= 4;
+		match *c {
+			b'A'...b'F' => b |= c - b'A' + 10,
+			b'a'...b'f' => b |= c - b'a' + 10,
+			b'0'...b'9' => b |= c - b'0',
+			_ => return None,
+		}
+		if (idx & 1) == 1 {
+			data.push(b);
+			b = 0;
+		}
+	}
+	match PublicKey::from_slice(&Secp256k1::without_caps(), &data) {
+		Ok(pk) => Some(pk),
+		Err(_) => None,
+	}
 }
 
 struct FeeEstimator {
@@ -91,7 +115,8 @@ impl chaininterface::BroadcasterInterface for ChainInterface {
 }
 
 struct Connection {
-	writer: Option<futures::stream::SplitSink<tokio_io::codec::Framed<tokio::net::TcpStream, tokio_codec::BytesCodec>>>,
+	writer: Option<futures::stream::SplitSink<tokio_codec::Framed<tokio::net::TcpStream, tokio_codec::BytesCodec>>>,
+	event_notify: mpsc::UnboundedSender<()>,
 	pending_read: Vec<u8>,
 	read_blocker: Option<futures::sync::oneshot::Sender<Result<(), std::io::Error>>>,
 	read_paused: bool,
@@ -99,53 +124,71 @@ struct Connection {
 	fd: RawFd,
 }
 impl Connection {
-	fn setup(peer_manager: Arc<peer_handler::PeerManager<SocketDescriptor>>, stream: tokio::net::TcpStream) {
-		let fd = stream.as_raw_fd();
-		let (writer, reader) = stream.framed(tokio_codec::BytesCodec::new()).split();
-		let us = Arc::new(Mutex::new(Self { writer: Some(writer), pending_read: Vec::new(), read_blocker: None, read_paused: true, need_disconnect: true, fd }));
-
-		let peer_manager_ref = peer_manager.clone();
+	fn schedule_read(peer_manager: Arc<peer_handler::PeerManager<SocketDescriptor>>, us: Arc<Mutex<Self>>, reader: futures::stream::SplitStream<tokio_codec::Framed<tokio::net::TcpStream, tokio_codec::BytesCodec>>) {
 		let us_ref = us.clone();
-		if let Ok(_) = peer_manager.new_inbound_connection(SocketDescriptor::new(us.clone(), peer_manager.clone())) {
-			tokio::spawn(reader.for_each(move |b| {
-				let unwrapper = |res: Result<Result<(), std::io::Error>, futures::sync::oneshot::Canceled>| { res.unwrap() };
-				let pending_read = b.to_vec();
-				{
-					let mut lock = us_ref.lock().unwrap();
-					assert!(lock.pending_read.is_empty());
-					if lock.read_paused {
-						lock.pending_read = pending_read;
-						let (sender, blocker) = futures::sync::oneshot::channel();
-						lock.read_blocker = Some(sender);
-						return blocker.then(unwrapper);
-					}
+		let us_close_ref = us.clone();
+		let peer_manager_ref = peer_manager.clone();
+		tokio::spawn(reader.for_each(move |b| {
+			let unwrapper = |res: Result<Result<(), std::io::Error>, futures::sync::oneshot::Canceled>| { res.unwrap() };
+			let pending_read = b.to_vec();
+			{
+				let mut lock = us_ref.lock().unwrap();
+				assert!(lock.pending_read.is_empty());
+				if lock.read_paused {
+					lock.pending_read = pending_read;
+					let (sender, blocker) = futures::sync::oneshot::channel();
+					lock.read_blocker = Some(sender);
+					return blocker.then(unwrapper);
 				}
-				match peer_manager_ref.read_event(&mut SocketDescriptor::new(us_ref.clone(), peer_manager_ref.clone()), pending_read) {
-					Ok(pause_read) => {
-						if pause_read {
-							let mut lock = us_ref.lock().unwrap();
-							lock.read_paused = true;
-						}
-					},
-					Err(e) => {
-						us_ref.lock().unwrap().need_disconnect = false;
-						//TODO: This sucks, find a better way:
-						let (sender, blocker) = futures::sync::oneshot::channel();
-						sender.send(Err(std::io::Error::new(std::io::ErrorKind::InvalidData, e))).unwrap();
-						return blocker.then(unwrapper);
+			}
+			match peer_manager.read_event(&mut SocketDescriptor::new(us_ref.clone(), peer_manager.clone()), pending_read) {
+				Ok(pause_read) => {
+					if pause_read {
+						let mut lock = us_ref.lock().unwrap();
+						lock.read_paused = true;
 					}
+				},
+				Err(e) => {
+					us_ref.lock().unwrap().need_disconnect = false;
+					//TODO: This sucks, find a better way:
+					let (sender, blocker) = futures::sync::oneshot::channel();
+					sender.send(Err(std::io::Error::new(std::io::ErrorKind::InvalidData, e))).unwrap();
+					return blocker.then(unwrapper);
 				}
+			}
 
-				//TODO: This sucks, find a better way:
-				let (sender, blocker) = futures::sync::oneshot::channel();
-				sender.send(Ok(())).unwrap();
-				blocker.then(unwrapper)
-			}).then(move |_| {
-				if us.lock().unwrap().need_disconnect {
-					peer_manager.disconnect_event(&SocketDescriptor::new(us, peer_manager.clone()));
-				}
-				Ok(())
-			}));
+			let _ = us_ref.lock().unwrap().event_notify.start_send(());
+
+			//TODO: This sucks, find a better way:
+			let (sender, blocker) = futures::sync::oneshot::channel();
+			sender.send(Ok(())).unwrap();
+			blocker.then(unwrapper)
+		}).then(move |_| {
+			if us_close_ref.lock().unwrap().need_disconnect {
+				peer_manager_ref.disconnect_event(&SocketDescriptor::new(us_close_ref, peer_manager_ref.clone()));
+			}
+			Ok(())
+		}));
+	}
+
+	fn setup_inbound(peer_manager: Arc<peer_handler::PeerManager<SocketDescriptor>>, event_notify: mpsc::UnboundedSender<()>, stream: tokio::net::TcpStream) {
+		let fd = stream.as_raw_fd();
+		let (writer, reader) = tokio_codec::Framed::new(stream, tokio_codec::BytesCodec::new()).split();
+		let us = Arc::new(Mutex::new(Self { writer: Some(writer), event_notify, pending_read: Vec::new(), read_blocker: None, read_paused: true, need_disconnect: true, fd }));
+
+		if let Ok(_) = peer_manager.new_inbound_connection(SocketDescriptor::new(us.clone(), peer_manager.clone())) {
+			Self::schedule_read(peer_manager, us, reader);
+		}
+	}
+
+	fn setup_outbound(peer_manager: Arc<peer_handler::PeerManager<SocketDescriptor>>, event_notify: mpsc::UnboundedSender<()>, their_node_id: PublicKey, stream: tokio::net::TcpStream) {
+		let fd = stream.as_raw_fd();
+		let (writer, reader) = tokio_codec::Framed::new(stream, tokio_codec::BytesCodec::new()).split();
+		let us = Arc::new(Mutex::new(Self { writer: Some(writer), event_notify, pending_read: Vec::new(), read_blocker: None, read_paused: true, need_disconnect: true, fd }));
+
+		if let Ok(initial_send) = peer_manager.new_outbound_connection(their_node_id, SocketDescriptor::new(us.clone(), peer_manager.clone())) {
+			let _ = us.lock().unwrap().writer.as_mut().unwrap().start_send(bytes::Bytes::from(initial_send));
+			Self::schedule_read(peer_manager, us, reader);
 		}
 	}
 }
@@ -189,6 +232,7 @@ impl peer_handler::SocketDescriptor for SocketDescriptor {
 						sender.send(Ok(())).unwrap();
 					}
 					us.read_paused = false;
+					let _ = us.event_notify.start_send(());
 					Ok(())
 				}));
 			}
@@ -247,6 +291,27 @@ impl PartialEq for SocketDescriptor {
 impl Hash for SocketDescriptor {
 	fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
 		self.fd.hash(state);
+	}
+}
+
+struct EventHandler {
+	peer_manager: Arc<peer_handler::PeerManager<SocketDescriptor>>,
+}
+impl EventHandler {
+	fn setup(peer_manager: Arc<peer_handler::PeerManager<SocketDescriptor>>) -> mpsc::UnboundedSender<()> {
+		let us = Arc::new(Self { peer_manager });
+		let (sender, receiver) = mpsc::unbounded();
+		tokio::spawn(receiver.for_each(move |_| {
+			us.peer_manager.process_events();
+			let events = us.peer_manager.get_and_clear_pending_events();
+			for event in events {
+				match event {
+					_ => unimplemented!(),
+				}
+			}
+			Ok(())
+		}).then(|_| { Ok(()) }));
+		sender
 	}
 }
 
@@ -313,11 +378,14 @@ fn main() {
 			route_handler: router,
 		}, our_node_secret));
 
+		let event_notify = EventHandler::setup(peer_manager.clone());
+
 		let listener = tokio::net::TcpListener::bind(&"0.0.0.0:9735".parse().unwrap()).unwrap();
 
 		let peer_manager_listener = peer_manager.clone();
+		let event_listener = event_notify.clone();
 		tokio::spawn(listener.incoming().for_each(move |sock| {
-			Connection::setup(peer_manager_listener.clone(), sock);
+			Connection::setup_inbound(peer_manager_listener.clone(), event_listener.clone(), sock);
 			Ok(())
 		}).then(|_| { Ok(()) }));
 
@@ -348,6 +416,43 @@ fn main() {
 		tokio::spawn(tokio::timer::Interval::new(Instant::now(), Duration::new(1, 0)).for_each(move |_| {
 			//TODO: Blocked on adding txn broadcasting to rest interface:
 			//      Regularly poll chain_monitor.txn_to_broadcast and send them out
+			Ok(())
+		}).then(|_| { Ok(()) }));
+
+		println!("Started interactive shell! Commands:");
+		println!("'c pubkey@host:port' Connect to given host+port, with given pubkey for auth");
+		print!("> "); std::io::stdout().flush().unwrap();
+		tokio::spawn(tokio_codec::FramedRead::new(tokio_fs::stdin(), tokio_codec::LinesCodec::new()).for_each(move |line| {
+			if line.len() > 2 && line.as_bytes()[1] == ' ' as u8 {
+				match line.as_bytes()[0] {
+					0x63 => { // 'c'
+						match hex_to_compressed_pubkey(line.split_at(2).1) {
+							Some(pk) => {
+								if line.as_bytes()[2 + 33*2] == '@' as u8 {
+									let parse_res: Result<std::net::SocketAddr, _> = line.split_at(2 + 33*2 + 1).1.parse();
+									if let Ok(addr) = parse_res {
+										print!("Attempting to connect to {}...", addr);
+										match std::net::TcpStream::connect(addr) {
+											Ok(stream) => {
+												println!("connected, initiating handshake!");
+												Connection::setup_outbound(peer_manager.clone(), event_notify.clone(), pk, tokio::net::TcpStream::from_std(stream, &tokio::reactor::Handle::current()).unwrap());
+											},
+											Err(e) => {
+												println!("connection failed {:?}!", e);
+											}
+										}
+									} else { println!("Couldn't parse host:port into a socket address"); }
+								} else { println!("Invalid line, should be c pubkey@host:port"); }
+							},
+							None => println!("Bad PubKey for remote node"),
+						}
+					},
+					_ => println!("Unknown command: {}", line.as_bytes()[0] as char),
+				}
+			} else {
+				println!("Unknown command line: {}", line);
+			}
+			print!("> "); std::io::stdout().flush().unwrap();
 			Ok(())
 		}).then(|_| { Ok(()) }));
 
