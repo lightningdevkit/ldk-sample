85a7f6b42ae4494a85138f8d0b2199784fac75c3
Matt Corallo <git@bluematt.me>
2020-03-04 12:08:27 -0500

    Build regtest invoices

--- src/main.rs
+++ src/main.rs
@@ -794,7 +794,7 @@ async fn main() {
 						let invoice_res = lightning_invoice::InvoiceBuilder::new(match network {
 								constants::Network::Bitcoin => lightning_invoice::Currency::Bitcoin,
 								constants::Network::Testnet => lightning_invoice::Currency::BitcoinTestnet,
-								constants::Network::Regtest => lightning_invoice::Currency::BitcoinTestnet, //TODO
+								constants::Network::Regtest => lightning_invoice::Currency::Regtest,
 							}).payment_hash(payment_hash).description("rust-lightning-bitcoinrpc invoice".to_string())
 							//.route(chans)
 							.amount_pico_btc(value * 10)
