cae92e8b151d8459fee7a7eb0b823c25951363f5
Matt Corallo <git@bluematt.me>
2021-05-04 17:44:04 +0000

    Check that the node we connect to matches the user-provided chain

--- src/convert.rs
+++ src/convert.rs
@@ -72,6 +72,7 @@ impl TryInto<FeeResponse> for JsonResponse {
 pub struct BlockchainInfo {
 	pub latest_height: usize,
 	pub latest_blockhash: BlockHash,
+	pub chain: String,
 }
 
 impl TryInto<BlockchainInfo> for JsonResponse {
@@ -81,6 +82,7 @@ impl TryInto<BlockchainInfo> for JsonResponse {
 			latest_height: self.0["blocks"].as_u64().unwrap() as usize,
 			latest_blockhash: BlockHash::from_hex(self.0["bestblockhash"].as_str().unwrap())
 				.unwrap(),
+			chain: self.0["chain"].as_str().unwrap().to_string(),
 		})
 	}
 }
