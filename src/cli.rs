ad6620d59606da10a3366efc569079b4512576ca
Matt Corallo <git@bluematt.me>
2025-09-30 09:38:39 +0000

    Drop `tokio` blocking now that `poll_for_user_input` is async

--- src/cli.rs
+++ src/cli.rs
@@ -111,12 +111,8 @@ pub(crate) async fn poll_for_user_input(
 								},
 							};
 
-						if tokio::runtime::Handle::current()
-							.block_on(connect_peer_if_necessary(
-								pubkey,
-								peer_addr,
-								peer_manager.clone(),
-							))
+						if connect_peer_if_necessary(pubkey, peer_addr, peer_manager.clone())
+							.await
 							.is_err()
 						{
 							continue;
@@ -438,12 +434,8 @@ pub(crate) async fn poll_for_user_input(
 								continue;
 							},
 						};
-					if tokio::runtime::Handle::current()
-						.block_on(connect_peer_if_necessary(
-							pubkey,
-							peer_addr,
-							peer_manager.clone(),
-						))
+					if connect_peer_if_necessary(pubkey, peer_addr, peer_manager.clone())
+						.await
 						.is_ok()
 					{
 						println!("SUCCESS: connected to peer {}", pubkey);
